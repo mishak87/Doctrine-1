e12f7d863de0cb80aabd5f499fe99c41c368c0be
Tomas Votruba <info@tomasvotruba.cz>
2014-04-14 11:19:29 +0200

    reverted

---
+++ src/Zenify/Doctrine/Facade.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * This file is part of Zenify
+ * Copyright (c) 2012 Tomas Votruba (http://tomasvotruba.cz)
+ */
+
+namespace Zenify\Doctrine;
+
+use Kdyby\Doctrine\Entities\BaseEntity;
+use Kdyby\Doctrine\EntityDao;
+use Nette;
+
+
+abstract class Facade extends Nette\Object
+{
+	/** @var EntityDao */
+	protected $dao;
+
+	/** @var Nette\DI\Container */
+	protected $container;
+
+
+	public function __construct(EntityDao $dao, Nette\DI\Container $container)
+	{
+		$this->dao = $dao;
+		$this->container = $container;
+	}
+
+
+	/**
+	 * @param  string
+	 * @param  string[]
+	 * @return mixed
+	 */
+	public function __call($name, $args = [])
+	{
+		if ( ! method_exists($this, $name) && method_exists($this->dao, $name)) {
+			$result = call_user_func_array(array($this->dao, $name), $args);
+
+			if (is_array($result)) {
+				foreach ($result as $entity) {
+					if (is_object($entity)) {
+						$this->container->callInjects($entity);
+					}
+				}
+			}
+
+			return $result;
+		}
+	}
+
+
+	/**
+	 * @return array
+	 */
+	public function findPairs($criteria, $value = NULL, $orderBy = [], $key = NULL)
+	{
+		if ( ! is_array($criteria)) {
+			$key = $orderBy;
+			$orderBy = [$criteria => 'ASC'];
+			$value = $criteria;
+			$criteria = [];
+		}
+
+		return $this->dao->findPairs($criteria, $value, $orderBy, $key);
+	}
+
+
+	/**
+	 * @param  int|BaseEntity
+	 */
+	public function delete($entity)
+	{
+		if (is_numeric($entity)) {
+			$entity = $this->getEntityManager()->find($this->getEntityName(), $entity);
+		}
+
+		$this->dao->delete($entity);
+	}
+
+
+	/**
+	 * @return string
+	 */
+	protected function getEntityName()
+	{
+		return $this->dao->getClassName();
+	}
+
+}
